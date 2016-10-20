aa43ec2b3d2abe6ec335039e07e2118d9fa5fa68
wddqing <wddqing@gmail.com>
2016-10-20 23:05:24 +0800

    temp

--- src/main/java/com/mcelf/serverselector/command/ServerListCommandExecutor.java
+++ src/main/java/com/mcelf/serverselector/command/ServerListCommandExecutor.java
@@ -1,10 +1,14 @@
 package com.mcelf.serverselector.command;
 
 import com.mcelf.serverselector.ServerSelector;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
@@ -24,7 +28,10 @@ public class ServerListCommandExecutor implements CommandExecutor {
 
         if(sender instanceof Player) {
 
-
+            Inventory inventory = Bukkit.createInventory(null,27,"test inventory");
+            ItemStack item = new ItemStack(Material.WOOL,1);
+            inventory.setItem(0,item);
+            ((Player) sender).openInventory(inventory);
             return true;
         }
         return false;
